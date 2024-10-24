057bcb79554e273dc4d28626ef59bc1fc2f736a7
Yasel <yaselbarrioscarrillo@gmail.com>
2024-10-24 15:38:09 -0400

    Cambiar la imagen del botón artículos cuando se presiona

--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -4,14 +4,17 @@ import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/ro
 @Component({
   selector: 'app-buttons',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.scss',
 })
 export class ButtonsComponent implements OnInit {
   isLogined = false;
+  articleIcon = 'assets/icons/gafas.png';  // Imagen por defecto para "Artículos"
+  selectedButton: string = '';  // Almacena el botón seleccionado
 
   private router = inject(Router);
+  
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -19,4 +22,16 @@ export class ButtonsComponent implements OnInit {
       }
     });
   }
+
+  // Método para cambiar el icono del botón seleccionado
+  changeIcon(selectedButton: string) {
+    this.selectedButton = selectedButton;  // Guardar el botón seleccionado
+    console.log(this.selectedButton);
+    if (selectedButton === 'article') {
+      this.articleIcon = 'assets/buttons/gafas 1.png';  // Cambiar a gafa 3 cuando se selecciona "Artículos"
+    } else {
+      this.articleIcon = 'assets/icons/gafas.png';  // Cambiar a gafas.png cuando se selecciona otro botón
+    }
+  }
 }
+
