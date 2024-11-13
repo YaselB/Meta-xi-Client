26ca6cb22661c7fa5d98e457fc0e8f98057316df
Yasel <yaselbarrioscarrillo@gmail.com>
2024-11-13 12:54:31 -0500

    Cambios en el proceso de recarga

--- src/app/common/deposit/address/fileupload/fileupload.component.ts
+++ src/app/common/deposit/address/fileupload/fileupload.component.ts
@@ -23,13 +23,14 @@ export class FileuploadComponent {
   onFileSelected(event: any){
     const file = event.target.files[0];
     const validImageTypes = ['image/png' , 'image/jpeg' ,'image/webp'];
-
     if(file && validImageTypes.includes(file.type)){
       this.fileName = file.name;
       this.selectedFile = file;
       const username = localStorage.getItem('username');
       const caption = `Usuario : ${username}\nToken: ${this.token}`
-      this.telegramService.sendPhoto(file ,caption);
+      console.log(file);
+      this.telegramService.sendPhotoToChannel(file ,caption );
+      console.log(file);
       this.notification.correct('Archivo enviado correctamente');
     }else{
       this.notification.errorMessage('Tipo de archivo no válido. Solo se permiten archivos PNG, JPG y WEBP.');
--- src/app/services/products/Telegram.service.ts
+++ src/app/services/products/Telegram.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class TelegramService {
-    private botToken = '7684550352:AAHcGOi4VM6kqqxfxpUpcYpLIxSkquuX1OY';
-    private chatId = '1425847313';
-    private telegramApiUrl = `https://api.telegram.org/bot${this.botToken}/sendPhoto`;
+  private botToken = '7684550352:AAHcGOi4VM6kqqxfxpUpcYpLIxSkquuX1OY';
+  chatid = "1425847313";
+  private telegramApiUrl = `https://api.telegram.org/bot${this.botToken}/sendPhoto`;
 
-    constructor(private http: HttpClient) { }
-    sendPhoto(photo : File ,caption : string): void {
-        const formData = new FormData();
-        formData.append('chat_id', this.chatId);
-        formData.append('photo', photo);
-        formData.append('caption', caption);
-        this.http.post(this.telegramApiUrl, formData).subscribe({
-            next: () => console.log('Photo sent successfully'),
-            error: (err) => console.error('Error sending photo', err),
-        });
-    }
+  constructor(private http: HttpClient) { }
+
+  sendPhotoToChannel(photo: File, caption: string ): void {
+    const formData = new FormData();
+    formData.append('chat_id', this.chatid);
+    formData.append('photo', photo);
+    formData.append('caption', caption);
+
+    this.http.post(this.telegramApiUrl, formData).subscribe({
+      next: (response) => console.log('Photo sent successfully', response),
+      error: (err) => console.error('Error sending photo', err),
+    });
+  }
 }
+
