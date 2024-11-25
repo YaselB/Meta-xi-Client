05106ebefe56b66c359534bb70946b8347575218
Yasel <yaselbarrioscarrillo@gmail.com>
2024-11-25 11:42:46 -0500

    Actualizar enlace de referidos

--- src/app/components/team/components/invitation/invitation.component.ts
+++ src/app/components/team/components/invitation/invitation.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-invitation',
@@ -8,9 +10,45 @@ import { Component } from '@angular/core';
   styleUrl: './invitation.component.scss',
 })
 export class InvitationComponent {
-  code = '703240';
-  link = 'https://ai-redbullvip.com/#/register?ic=703240';
+  code : string = '';
+  link : string = '';
+  username: string = localStorage.getItem('username') || '';    
 
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.GetCode();
+  }
+  async GetCode() {
+    try {
+      this.link = await this.GettingCode();
+      this.code = this.extractCodeFromUrl(this.link);
+      console.log(this.link);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+  async GettingCode() : Promise<any> {
+    const url = 'https://meta-xi-api-production.up.railway.app/api/User/GetLink/'+this.username;
+    try {
+      const response : any = await firstValueFrom(this.http.get(url));
+      console.log(response.link)
+      return response.link;
+    } catch (error : any) {
+      let errorMessage = '';
+      if (error.error ) {
+        if(typeof error.error === 'string') {
+          errorMessage = error.error;
+        } else if(error.error.message) {
+          errorMessage = error.error.message;
+        }
+        return errorMessage;
+      }
+    }
+  }
+  private extractCodeFromUrl(url: string): string {
+    const urlParams = new URL(url).searchParams;
+    return urlParams.get('code') || '';
+  }
   copyToClipboard(text: string, button: HTMLButtonElement): void {
     navigator.clipboard
       .writeText(text)
