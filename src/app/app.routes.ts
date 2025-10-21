5f1d6721beee7d4f2939dcfb7c82716f46db6cfe
YaselB <yaselbarrioscarrillo@gmail.com>
2025-10-21 10:11:44 -0400

    eliminando errores de funciones para validacion en el formulaio de login y quitando proteccion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () =>
       import('./components/home/home.component').then((m) => m.HomeComponent),
-    canActivate: [authGuard],
+    
   },
   {
     path: 'tasks',
@@ -21,14 +21,12 @@ export const routes: Routes = [
       import('./components/glasses/glasses.component').then(
         (m) => m.TasksComponent
       ),
-    canActivate: [authGuard]
-  },
+      },
   {
     path: 'team',
     loadComponent: () =>
       import('./components/team/team.component').then((m) => m.TeamComponent),
-    canActivate: [authGuard]
-  },
+      },
   {
     path: 'lvl/:lvl',
     loadComponent: () =>
@@ -41,69 +39,60 @@ export const routes: Routes = [
     path: 'vip',
     loadComponent: () =>
       import('./components/vip/vip.component').then((m) => m.VipComponent),
-    canActivate: [authGuard]
-  },
+      },
   {
     path: 'me',
     loadComponent: () =>
       import('./components/me/me.component').then((m) => m.MeComponent),
-    canActivate: [authGuard]
-  },
+      },
   {
     path: 'recharge',
     loadComponent: () =>
       import('./common/recharge/recharge.component').then(
         (m) => m.RechargeComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: 'withdraw/:token',
     loadComponent: () =>
       import('./common/withdraw/withdraw.component').then(
         (m) => m.WithdrawComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: 'withdrawToken',
     loadComponent: () =>
       import('./common/withdrawToken/withdrawToken.component').then(
         (m) => m.WithdrawTokenComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: 'account',
     loadComponent: () =>
       import('./components/me/account/account.component').then(
         (m) => m.AccountComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: 'password',
     loadComponent: () =>
       import('./components/me/changepass/changepass.component').then(
         (m) => m.ChangepassComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: 'deposit/:token',
     loadComponent: () =>
       import('./common/deposit/deposit.component').then(
         (m) => m.DepositComponent
       ),
-      canActivate: [authGuard]
-  },{
+        },{
     path: 'nequi/:token',
     loadComponent: ()=>
       import('./common/deposit/address/nequi-confirmation/nequi-confirmation.component').then(
         (m) => m.NequiConfirmationComponent
       ),
-      canActivate: [authGuard]
-  },
+        },
   {
     path: '**',
     redirectTo: 'login',
